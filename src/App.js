823716506f87169a998bd72102a53a00f1d41133
mateohssj <mateohenao206@gmail.com>
2022-05-12 16:13:49 -0500

    aprendiendo react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import {Buttom, ButtomUi} from "./components/Ui/ButtomUI/Buttom"
 
 function App() {
   return (
     <div className="App">
       <h1>Hola</h1>
       <h2>soy jefferson</h2>
+      <button>ButtomUI</button>
     </div>
   );
 }
---
+++ src/components/Ui/ButtomUI/Buttom.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Buttom = () => {
+  return (
+    <div>Buttom</div>
+  )
+}
